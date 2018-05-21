cc526442c4c9f9520f58de288de90842cb952355
Emre <eay@kth.se>
2018-05-21 21:58:55 +0100

    Specific transaction converters

--- bank_statement_wizard/constants_and_types.py
+++ bank_statement_wizard/constants_and_types.py
@@ -1,4 +1,4 @@
-from .utility import create_new_entry_type_and_csv_parser
+from .utility import create_new_entry_type_and_csv_parser, StatementEntryToTransactionConverter
 
 
 lloyds_account_statement_fields = ['transaction_date', 'transaction_type', 'sort_code',
@@ -16,8 +16,20 @@ LloydsAccountStatementEntry, LloydsAccountStatementParser = \
         csv_fields=lloyds_account_statement_fields
     )
 
+
+LloydsAccountsStatementEntryToTransaction = StatementEntryToTransactionConverter(
+    ['transaction_date', 'debit_amount',
+        'transaction_description', 'transaction_type']
+)
+
+
 LloydsCreditCardStatementEntry, LloydsCreditCardStatementParser = \
     create_new_entry_type_and_csv_parser(
         type_name='LloydsCreditCardStatementEntry',
         csv_fields=lloyds_credit_card_statement_fields
     )
+
+
+LloydsCreditCardStatementEntryToTranscation = StatementEntryToTransactionConverter(
+    ['date', 'amount', 'description', 'reference']
+)
