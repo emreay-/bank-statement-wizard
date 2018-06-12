3483f51bda70f7778d439aaf91c8c1970b3c3179
Emre <eay@kth.se>
2018-06-12 23:02:49 +0100

    Categorizing transactions in the ledger, and getter function for a given category

--- bank_statement_wizard/ledger.py
+++ bank_statement_wizard/ledger.py
@@ -1,7 +1,17 @@
-from typing import List
-
+from typing import List, Dict
 
 from .utility import Transaction
+
+
+def categorize_transactions(transactions: List[Transaction]) -> Dict[str, List[Transaction]]:
+    categorized_transactions = {}
+
+    for t in transactions:
+        if t.transaction_category not in categorized_transactions:
+            categorized_transactions[t.transaction_category] = []
+        categorized_transactions[t.transaction_category].append(t)
+
+    return categorized_transactions
 
 
 class Ledger:
@@ -15,6 +25,7 @@ class Ledger:
 
     def run(self, transactions: List[Transaction]) -> 'Ledger':
         self.separate_debit_and_credit_transactions(transactions)
+        self.categorize_transactions()
         self.update_balance()
         return self
 
@@ -25,12 +36,29 @@ class Ledger:
             elif transaction.credit_amount != None:
                 self.credit_transactions.append(transaction)
 
+    def categorize_transactions(self):
+        self.categorized_debit_transactions = categorize_transactions(
+            self.debit_transactions)
+        self.categorized_credit_transactions = categorize_transactions(
+            self.credit_transactions)
+
     def update_balance(self):
         self.debit_balance = sum(
             [transaction.debit_amount for transaction in self.debit_transactions])
         self.credit_balance = sum(
             [transaction.credit_amount for transaction in self.credit_transactions])
         self.balance = self.credit_balance - self.debit_balance
+
+    def get_transactions_with_category(self, category: str) -> List[Transaction]:
+        transactions = []
+        if category in self.categorized_credit_transactions:
+            transactions += self.categorized_credit_transactions[category]
+        if category in self.categorized_debit_transactions:
+            transactions += self.categorized_debit_transactions[category]
+        return transactions
+
+    def generate_report(self) -> 'Ledger':
+        pass
 
     def __str__(self):
         return '''Debit Balance   : {}
