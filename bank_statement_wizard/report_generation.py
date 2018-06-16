7574f086f8751f974570e3ad8c386518e8c84cac
Emre <eay@kth.se>
2018-06-16 23:11:05 +0100

    Updates on report, now category stats table and a pie chart are generated

--- bank_statement_wizard/report_generation.py
+++ bank_statement_wizard/report_generation.py
@@ -1,12 +1,13 @@
 import os
-from typing import Optional
+from typing import Optional, Tuple
 
 from reportlab.pdfgen import canvas
 from reportlab.lib import colors
 from reportlab.lib.pagesizes import letter
 from reportlab.lib.enums import TA_LEFT
 from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph
-from reportlab.lib.styles import ParagraphStyle
+from reportlab.graphics.shapes import Drawing
+from reportlab.graphics.charts.piecharts import Pie, Rect
 
 from .ledger import Ledger
 
@@ -18,12 +19,21 @@ def check_font(font: str):
 class StatementReportGenerator:
 
     _default_font = 'Courier'
+    _default_page_size = letter
+    _default_margin = 72
 
-    def __init__(self, font: Optional[str] = None):
+    def __init__(
+        self,
+        font: Optional[str] = None,
+        page_size: Optional[Tuple[float, float]] = None,
+        margin: Optional[int] = None
+    ):
         self._report_elements = []
         self._report_path = None
         self._document = None
         self._font = font if check_font(font) else self._default_font
+        self._page_size = page_size if page_size else self._default_page_size
+        self._margin = margin if margin != None else self._default_margin
 
     def __call__(
         self,
@@ -34,6 +44,8 @@ class StatementReportGenerator:
     ):
         self._add_info_table(statement_type, statement_date)
         self._add_transactions_table(ledger)
+        self._add_category_stats(ledger)
+        self._add_pie_chart(ledger, size=self.width*0.45, padding=0)
 
         self._set_report_path(path_to_output_dir,
                               statement_type, statement_date)
@@ -53,18 +65,53 @@ class StatementReportGenerator:
         transactions_data = title + \
             [(i.date, i.description, i.amount, i.transaction_category)
              for i in ledger]
-        transactions_table = Table(transactions_data)
+        transactions_table = Table(transactions_data, spaceAfter=40)
         transactions_table.setStyle(TableStyle([('FONTNAME', (0, 0), (-1, 0), self.font_bold),
                                                 ('FONTNAME', (0, 1), (-1, -1), self.font)]))
         self.report_elements.append(transactions_table)
+
+    def _add_category_stats(self, ledger: Ledger):
+        title = [('Category', 'Total Amount', 'Percentage')]
+        data = [(category, '{:.2f}'.format(total), '{:.2f} %'.format(percentage * 100))
+                for category, (total, percentage) in ledger.get_expense_stats().items()]
+        data.sort(key=lambda x: float(x[1]), reverse=True)
+        stats_table = Table(title + data, spaceAfter=40)
+        stats_table.setStyle(TableStyle([('FONTNAME', (0, 0), (-1, 0), self.font_bold),
+                                         ('FONTNAME', (0, 1), (-1, -1), self.font)]))
+        self.report_elements.append(stats_table)
+
+    def _add_pie_chart(
+            self,
+            ledger: Ledger,
+            size: int,
+            padding: int,
+    ):
+        figure = Drawing(self.width, min(size + 2 * padding, self.height))
+        pie_chart = Pie()
+        pie_chart.x = (self.width - size) / 2
+        pie_chart.y = padding
+        pie_chart.width = size
+        pie_chart.height = size
+        pie_chart.data = [i[0] for i in ledger.get_expense_stats().values()]
+        pie_chart.labels = list(ledger.get_expense_stats().keys())
+        pie_chart.slices.strokeWidth = 0.5
+        pie_chart.sideLabels = True
+        figure.add(pie_chart)
+        self.report_elements.append(figure)
 
     def _set_report_path(self, path_to_output_dir: str, statement_type: str, statement_date: str):
         self.report_path = os.path.join(path_to_output_dir, '{}_report_{}.pdf'.format(
             statement_type, statement_date))
 
     def _create_document(self):
-        self.document = SimpleDocTemplate(self.report_path, rightMargin=72, leftMargin=72,
-                                          topMargin=72, bottomMargin=72, pagesize=letter)
+        self.document = SimpleDocTemplate(
+            self.report_path,
+            rightMargin=self.margin,
+            leftMargin=self.margin,
+            topMargin=self.margin,
+            bottomMargin=self.margin,
+            pagesize=self._default_page_size
+        )
 
     def _build(self):
         self.document.build(self.report_elements)
@@ -96,3 +143,15 @@ class StatementReportGenerator:
     @property
     def font_bold(self):
         return '{}-Bold'.format(self.font)
+
+    @property
+    def margin(self):
+        return self._margin
+
+    @property
+    def width(self):
+        return self._page_size[0] - 2 * self.margin - 12
+
+    @property
+    def height(self):
+        return self._page_size[1] - 2 * self.margin - 12
