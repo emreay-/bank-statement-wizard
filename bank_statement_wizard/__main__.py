b3e097ae1837e158f01545e1302716aab8ac68e7
Emre <eay@kth.se>
2018-06-16 20:29:42 +0100

    New program options are added, generating a pdf report now

--- bank_statement_wizard/__main__.py
+++ bank_statement_wizard/__main__.py
@@ -1,9 +1,17 @@
 import argparse
 
-from .utility import load_json_file, create_named_tuple_with_name_and_fields
+from .utility import load_json_file, create_named_tuple_with_name_and_fields, check_date
 from .analysis import SimpleExpenseCategoryMatcher, regex_search_score
 from .constants_and_types import accepted_statements
 from .ledger import Ledger
+from .report_generation import StatementReportGenerator
+
+
+def date_type(date):
+    if not check_date(date):
+        raise argparse.ArgumentTypeError(
+            '{} is not a valid date, it should be in dd-mm-yyyy format'.format(date))
+    return date
 
 
 def parse_arguments():
@@ -14,15 +22,21 @@ def parse_arguments():
                         help='Path to statement in csv file', required=True)
     parser.add_argument('-t', '--type',
                         choices=list(accepted_statements.keys()), help='Statement type', required=True)
+    parser.add_argument('-d', '--date', help='Statement date in dd-mm-yyyy format',
+                        type=date_type, required=True)
+    parser.add_argument('-o', '--output', help='Output directory path',
+                        required=True)
     args = parser.parse_args()
 
-    return args.expense_categories, args.statement, args.type
+    return args.expense_categories, args.statement, args.type, args.date, args.output
 
 
 def process_statement(
     path_to_expense_categories: str,
     path_to_statement_csv: str,
-    statement_type: str
+    statement_type: str,
+    statement_date: str,
+    path_to_output_dir: str
 ):
     statement_entries = accepted_statements[statement_type].parser(
         path_to_statement_csv
@@ -37,6 +51,13 @@ def process_statement(
 
     ledger = Ledger().run(transactions)
     print('\n{}\n\n'.format(ledger))
+
+    StatementReportGenerator()(
+        path_to_output_dir=path_to_output_dir,
+        statement_type=statement_type,
+        statement_date=statement_date,
+        ledger=ledger
+    )
 
 
 process_statement(*parse_arguments())
