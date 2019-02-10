88724a08dddc5aa0e205399defba05738b602cc8
Emre <eay@kth.se>
2019-02-10 19:28:41 +0000

    Made the conventions getter functions, created loader functions

--- bank_statement_wizard/parsing/lloyds_bank_uk.py
+++ bank_statement_wizard/parsing/lloyds_bank_uk.py
@@ -1,33 +1,39 @@
+from typing import List, Dict
 from collections import OrderedDict
 
 from bank_statement_wizard.ledger import Transaction
+from bank_statement_wizard.parsing.parsing_utility import parse_csv_using_schema
 from bank_statement_wizard.parsing.parsing_utility import get_line_parser_from_ordered_dict
 from bank_statement_wizard.parsing.parsing_utility import default_comment_char, default_delimiter
+from bank_statement_wizard.parsing.parsing_utility import to_stripped_string, optional_str_to_float
 
 
-current_account_statement_line_parser = get_line_parser_from_ordered_dict(
-    OrderedDict([
-        ('transaction_date', to_stripped_string),
-        ('transaction_type', to_stripped_string),
-        ('sort_code', to_stripped_string),
-        ('account_number', to_stripped_string),
-        ('transaction_description', to_stripped_string),
-        ('debit_amount', optional_str_to_float),
-        ('credit_amount', optional_str_to_float),
-        ('balance', optional_str_to_float)
-    ])
-)
+def get_current_account_statement_line_parser():
+    return get_line_parser_from_ordered_dict(
+        OrderedDict([
+            ('transaction_date', to_stripped_string),
+            ('transaction_type', to_stripped_string),
+            ('sort_code', to_stripped_string),
+            ('account_number', to_stripped_string),
+            ('transaction_description', to_stripped_string),
+            ('debit_amount', optional_str_to_float),
+            ('credit_amount', optional_str_to_float),
+            ('balance', optional_str_to_float)
+        ])
+    )
 
 
-current_account_statement_schema = {
-    'delimiter': default_delimiter(),
-    'is_ignore_line': lambda s: s.startswith(default_comment_char()) or s.startswith('Transaction'),
-    'line_parser': current_account_statement_line_parser
-}
+def get_current_account_statement_schema():
+    return {
+        'delimiter': default_delimiter(),
+        'is_ignore_line': lambda s: s.startswith(default_comment_char()) or s.startswith('Transaction'),
+        'line_parser': get_current_account_statement_line_parser()
+    }
 
 
 def current_account_statement_entry_to_transaction(entry: Dict) -> Transaction:
-    _amount = entry['credit_amount'] if entry['credit_amount'] is not None else -1.0 * entry['debit_amount']
+    _amount = entry['credit_amount'] if entry['credit_amount'] is not None else - \
+        1.0 * entry['debit_amount']
 
     return Transaction(
         amount=_amount,
@@ -37,22 +43,31 @@ def current_account_statement_entry_to_transaction(entry: Dict) -> Transaction:
     )
 
 
-credit_card_statement_line_parser = get_line_parser_from_ordered_dict(
-    OrderedDict([
-        ('date', to_stripped_string),
-        ('date_entered', to_stripped_string),
-        ('reference', to_stripped_string),
-        ('description', to_stripped_string),
-        ('transaction_description', to_stripped_string),
-        ('amount', optional_str_to_float)
-    ])
-)
+def load_transactions_from_lloyds_bank_uk_current_account_statement(path_to_statement: str) -> List[Transaction]:
+    parsed_entries = parse_csv_using_schema(
+        path_to_statement, get_current_account_statement_schema())
+    return [current_account_statement_entry_to_transaction(i) for i in parsed_entries]
 
-credit_card_statement_schema = {
-    'delimiter': default_delimiter(),
-    'is_ignore_line': lambda s: s.startswith(default_comment_char()) or s.startswith('Date'),
-    'line_parser': credit_card_statement_line_parser
-}
+
+def get_credit_card_statement_line_parser():
+    return get_line_parser_from_ordered_dict(
+        OrderedDict([
+            ('date', to_stripped_string),
+            ('date_entered', to_stripped_string),
+            ('reference', to_stripped_string),
+            ('description', to_stripped_string),
+            ('transaction_description', to_stripped_string),
+            ('amount', optional_str_to_float)
+        ])
+    )
+
+
+def get_credit_card_statement_schema():
+    return {
+        'delimiter': default_delimiter(),
+        'is_ignore_line': lambda s: s.startswith(default_comment_char()) or s.startswith('Date'),
+        'line_parser': get_credit_card_statement_line_parser()
+    }
 
 
 def credit_card_statement_entry_to_transaction(entry: Dict) -> Transaction:
@@ -62,3 +77,9 @@ def credit_card_statement_entry_to_transaction(entry: Dict) -> Transaction:
         description=entry['description'],
         additional_info=entry['reference'],
     )
+
+
+def load_transactions_from_lloyds_bank_uk_credit_card_statement(path_to_statement: str) -> List[Transaction]:
+    parsed_entries = parse_csv_using_schema(
+        path_to_statement, get_credit_card_statement_schema())
+    return [credit_card_statement_entry_to_transaction(i) for i in parsed_entries]
