f678adb6eb08a4523162dca855e9b373bb376675
Emre <eay@kth.se>
2019-02-10 19:30:26 +0000

    New module to provide load functions for supported banks

---
+++ bank_statement_wizard/parsing/support.py
@@ -0,0 +1,22 @@
+from enum import Enum
+from typing import Callable, List
+
+from bank_statement_wizard.parsing.lloyds_bank_uk import load_transactions_from_lloyds_bank_uk_credit_card_statement
+from bank_statement_wizard.parsing.lloyds_bank_uk import load_transactions_from_lloyds_bank_uk_current_account_statement
+
+
+class SupportedStatementTypes(Enum):
+    LloydsBankUKCurrentAccountStatement = 'lloyds-debit'
+    LloydsBankUKCreditCardStatement = 'lloyds-credit'
+
+
+def statement_types() -> List[str]:
+    return [_type.value for _type in SupportedStatementTypes]
+
+
+def get_loader(statement_type: str) -> Callable:
+    if statement_type == SupportedStatementTypes.LloydsBankUKCurrentAccountStatement.value:
+        return load_transactions_from_lloyds_bank_uk_current_account_statement
+    elif statement_type == SupportedStatementTypes.LloydsBankUKCreditCardStatement.value:
+        return load_transactions_from_lloyds_bank_uk_credit_card_statement
+    raise ValueError('{} is not a valid statement type'.format(statement_type))
