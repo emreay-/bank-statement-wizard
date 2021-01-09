c2928cbadad6663bd508c08d95c2e822cd3d1194
Emre Ay <6861956+emreay-@users.noreply.github.com>
2021-01-09 01:01:49 +0000

    Allow passing multiple statements of the same type

--- src/bank_statement_wizard/__main__.py
+++ src/bank_statement_wizard/__main__.py
@@ -1,4 +1,5 @@
 import argparse
+from typing import List
 
 from bank_statement_wizard.domain.ledger import Ledger
 from bank_statement_wizard.domain.utility import load_category_data, check_date
@@ -8,46 +9,51 @@ from bank_statement_wizard.report_generation import StatementReportGenerator
 from bank_statement_wizard.parsing.support import get_loader, statement_types
 
 
+__all__ = ["main"]
+
+
 def date_type(date):
     if not check_date(date):
         raise argparse.ArgumentTypeError(
-            '{} is not a valid date, it should be in dd-mm-yyyy format'.format(date))
+            "{} is not a valid date, it should be in dd-mm-yyyy format".format(date))
     return date
 
 
 def parse_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-e', '--expense_categories',
-                        help='Json file with expense category keywords', required=True)
-    parser.add_argument('-s', '--statement',
-                        help='Path to statement in csv file', required=True)
-    parser.add_argument('-t', '--type',
-                        choices=statement_types(), help='Statement type', required=True)
-    parser.add_argument('-d', '--date', help='Statement date in dd-mm-yyyy format',
+    parser.add_argument("-e", "--expense_categories",
+                        help="Json file with expense category keywords", required=True)
+    parser.add_argument("-s", "--statements",
+                        help="Path to statement(s) in csv file", nargs="+")
+    parser.add_argument("-t", "--type",
+                        choices=statement_types(), help="Statement type", required=True)
+    parser.add_argument("-d", "--date", help="Statement date in dd-mm-yyyy format",
                         type=date_type, required=True)
-    parser.add_argument('-o', '--output', help='Output directory path',
+    parser.add_argument("-o", "--output", help="Output directory path",
                         required=True)
     args = parser.parse_args()
 
-    return args.expense_categories, args.statement, args.type, args.date, args.output
+    return args.expense_categories, args.statements, args.type, args.date, args.output
 
 
 def process_statement(
-    path_to_expense_categories: str,
-    path_to_statement_csv: str,
+    expense_categories_file: str,
+    statement_paths: List[str],
     statement_type: str,
     statement_date: str,
     path_to_output_dir: str
 ):
     ledger = Ledger()
-    for transaction in get_loader(statement_type)(path_to_statement_csv):
-        ledger.add_transaction(transaction)
 
-    expense_categories = load_category_data(path_to_expense_categories)
+    for statement in statement_paths:
+        transactions = get_loader(statement_type)(statement)
+        ledger.add_transactions(transactions)
+
+    expense_categories = load_category_data(expense_categories_file)
     matcher = SimpleExpenseCategoryMatcher(expense_categories)
     matcher.match_bulk(ledger.transactions)
 
-    print('\n{}\n'.format(ledger))
+    print("\n{}\n".format(ledger))
 
     if ledger.debit_balance > 0.0:
         grouped_debit_transactions = group_transactions_using_category(
@@ -66,4 +72,5 @@ def process_statement(
     )
 
 
-process_statement(*parse_arguments())
+def main():
+    process_statement(*parse_arguments())
